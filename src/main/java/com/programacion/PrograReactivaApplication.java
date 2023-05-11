86e5787851cafbdf55673d3ed2985cffd44b4d47
Xavier Aguilar <kevinjkevps4@gmail.com>
2023-05-11 16:36:50 -0500

    test

---
+++ src/main/java/com/programacion/PrograReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.programacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrograReactivaApplication.class, args);
+	}
+
+}
